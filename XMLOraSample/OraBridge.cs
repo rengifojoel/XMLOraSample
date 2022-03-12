feb186c34df87bb29ddee7e55e8a42f5f4034fbf
rengifojoel <rengifojoel@IP-C613562A>
2022-03-12 18:59:28 -0400

    Add project files.

---
+++ XMLOraSample/OraBridge.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace XMLOraSample
+{
+    public class OraBridge
+    {
+        public static void passXmlAsCLOB (string content, ref string successProof)
+        {
+            Oracle.ManagedDataAccess.Client.OracleConnection conn = new Oracle.ManagedDataAccess.Client.OracleConnection("YOUR CONNECTION_STRING") ;
+            conn.Open();
+            Oracle.ManagedDataAccess.Client.OracleCommand cmd = new Oracle.ManagedDataAccess.Client.OracleCommand();
+            cmd.CommandText = "XMLHANDLER.XMLHANDLERWITHCLOBPARAM";
+            cmd.Connection = conn;
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            Oracle.ManagedDataAccess.Client.OracleParameter inputParam = cmd.Parameters.Add("XMLCONTENT", Oracle.ManagedDataAccess.Client.OracleDbType.Clob);
+            inputParam.Direction = System.Data.ParameterDirection.Input;
+            inputParam.Value = content;
+            Oracle.ManagedDataAccess.Client.OracleParameter outParam = cmd.Parameters.Add("SUCCESSPROOF", Oracle.ManagedDataAccess.Client.OracleDbType.Varchar2,100);
+            outParam.Direction = System.Data.ParameterDirection.Output;
+            cmd.ExecuteNonQuery();
+            successProof = cmd.Parameters["SUCCESSPROOF"].Value.ToString();
+            conn.Close();
+        }
+        public static void passXmlAsXML(System.Xml.XmlDocument content, ref string successProof)
+        {
+            Oracle.ManagedDataAccess.Client.OracleConnection conn = new Oracle.ManagedDataAccess.Client.OracleConnection("YOUR_CONNECTION_STRING");
+            conn.Open();
+            Oracle.ManagedDataAccess.Client.OracleCommand cmd = new Oracle.ManagedDataAccess.Client.OracleCommand();
+            cmd.CommandText = "XMLHANDLER.XMLHANDLERWITHXMLPARAM";
+            cmd.Connection = conn;
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            Oracle.ManagedDataAccess.Client.OracleParameter inputParam = cmd.Parameters.Add("XMLCONTENT", Oracle.ManagedDataAccess.Client.OracleDbType.XmlType);
+            inputParam.Direction = System.Data.ParameterDirection.Input;
+
+            inputParam.Value = content.OuterXml;
+            Oracle.ManagedDataAccess.Client.OracleParameter outParam = cmd.Parameters.Add("SUCCESSPROOF", Oracle.ManagedDataAccess.Client.OracleDbType.Varchar2, 100);
+            outParam.Direction = System.Data.ParameterDirection.Output;
+            cmd.ExecuteNonQuery();
+            successProof = cmd.Parameters["SUCCESSPROOF"].Value.ToString();
+            conn.Close();
+        }
+
+    }
+}
---
+++ XMLOraSampleTests1/OraBridgeTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XMLOraSample.Tests
+{
+    [TestClass()]
+    public class OraBridgeTests
+    {
+        [TestMethod()]
+        public void passXmlAsCLOBTest()
+        {
+            string xml = System.IO.File.ReadAllText("input.xml");
+            string returnValue = string.Empty;
+            XMLOraSample.OraBridge.passXmlAsCLOB(xml, ref returnValue);
+            Assert.IsTrue( returnValue == "http://www.twitter.com");
+        }
+        [TestMethod()]
+        public void passXmlAsXMLTest()
+        {
+            System.Xml.XmlDocument xml= new System.Xml.XmlDocument ();
+            xml.LoadXml(System.IO.File.ReadAllText("input.xml"));
+            string returnValue = string.Empty;
+            XMLOraSample.OraBridge.passXmlAsXML(xml, ref returnValue);
+            Assert.IsTrue(returnValue == "http://www.twitter.com");
+        }
+    }
+}
